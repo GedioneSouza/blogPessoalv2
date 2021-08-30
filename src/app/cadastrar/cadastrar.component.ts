fc94a5a02c863f62474630f4b4915b126b5483c8
GedioneSouza <gedionesouza2003@gmail.com>
2021-08-30 11:07:33 -0300

    Consumo de API - Login e Cadastro

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,9 @@
+import { AuthService } from '../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/User';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+  confirmarSenha: string
+  tipoUsuario: string
+  
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar(){
+     if(this.user.nome.length<3){
+       alert('Preencha o campo nome com pelo menos 3 caracteres')
+     }
+
+     if(this.user.usuario.length<3){
+       alert('Preencha o campo usuario com pelo menos 3 caracteres')
+     }
+    
+     if(this.user.usuario.indexOf('@') == -1 || this.user.usuario.indexOf('.') == -1){
+      alert('E-mail Inválido!')
+    }
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmarSenha){
+       alert('A senhas estão incorretas.')
+    } else {
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
+
   }
 
 }
