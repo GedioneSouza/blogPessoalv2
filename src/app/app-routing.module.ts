c34e8299e79f4d1dcd3ca8d02b465bbd3ecd7ea8
GedioneSouza <gedionesouza2003@gmail.com>
2021-09-03 09:32:04 -0300

    Implementação do put e delete de temas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 
 
@@ -16,6 +18,9 @@ const routes: Routes = [
 
  {path: 'inicio', component: InicioComponent},
  {path: 'tema', component: TemaComponent},
+
+ {path: 'tema-edit/:id', component: TemaEditComponent},
+ {path: 'tema-delete/:id', component: TemaDeleteComponent}
 
 ];
 
